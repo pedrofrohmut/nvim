cab0d18685253f7c6db657d1983ba4b5952ef47f
pedrofrohmut <pedrofrohmut@gmail.com>
2026-05-10 11:36:25 -0300

    Git Signs + Usefull git keybinds and Commands

---
+++ lua/plugins/git-signs.lua
@@ -0,0 +1,19 @@
+local gs = require("gitsigns")
+local map = vim.keymap.set
+local cmd = vim.api.nvim_create_user_command
+
+-- Navigation
+map("n", "]g", function() gs.nav_hunk("next") end, { desc = "Next git hunk" })
+map("n", "[g", function() gs.nav_hunk("prev") end, { desc = "Previous git hunk" })
+
+-- Manipulate Hunks
+map("n", "<leader>hp", function() gs.preview_hunk() end, { desc = "Preview hunk" })
+cmd("GSStageHunk", function() gs.stage_hunk() end, { desc = "Stage hunk" })
+cmd("GSResetHunk", function() gs.reset_hunk() end, { desc = "Reset hunk" })
+
+-- Blame
+map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, { desc = "Blame line" })
+map("n", "<leader>hB", function() gs.toggle_current_line_blame() end, { desc = "Toggle inline blame" })
+
+-- View Diff
+cmd("GSDiff", function() gs.diffthis() end, { desc = "Diff this" })
