a27067c744b86b4febcbf8dae4977a26945cd35e
pedrofrohmut <pedrofrohmut@gmail.com>
2026-05-17 15:34:54 -0300

    Testing eslint

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -9,7 +9,30 @@ lint.linters_by_ft = {
     python = { "pylint" },
 }
 
+local lint_enabled = false
+
+local toggler = function()
+    if lint_enabled then
+        vim.notify("Lint is turned off")
+        lint_enabled = false
+    else
+        vim.notify("Lint is turned on")
+        lint_enabled = true
+    end
+end
+
+vim.keymap.set("n", "<leader>cl", toggler, { desc = "Toggle Lint on Save" })
+
+-- Autocmd to run linter on file save
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    callback = function()
+        if lint_enabled then
+            lint.try_lint()
+        end
+    end,
+})
+
+-- Old stuff
 -- Autocmd to run linter on file save
 --vim.api.nvim_create_autocmd({ "BufWritePost" }, { callback = lint.try_lint })
-
-vim.keymap.set("n", "<leader>cl", lint.try_lint, { desc = "Run code Linter" })
+-- vim.keymap.set("n", "<leader>cl", lint.try_lint, { desc = "Run code Linter" })
